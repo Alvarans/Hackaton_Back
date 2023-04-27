c51cfe0b69d1f2fdce17650d684f548f22ca361e
Denic <dragonnestpvpe@gmail.com>
2023-04-27 11:52:47 +0300

    event repository change

---
+++ src/main/kotlin/ru/pnzgu/hackapp/repositories/EventRepository.kt
@@ -0,0 +1,12 @@
+package ru.pnzgu.hackapp.repositories
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import ru.pnzgu.hackapp.dto.EventDto
+import ru.pnzgu.hackapp.model.EventEntity
+
+@Repository
+interface EventRepository : JpaRepository<EventEntity, Long> {
+
+    fun findEventEntitiesByEventid(id: Long) : List<EventEntity>
+}
