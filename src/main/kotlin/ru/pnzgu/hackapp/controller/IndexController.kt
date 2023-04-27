2e91ce82e4714c3594ae437d268560d983842eb9
ilya <t1m0shenko@yandex.ru>
2023-04-27 12:20:59 +0300

    add toDto and api all events

--- src/main/kotlin/ru/pnzgu/hackapp/controller/IndexController.kt
+++ src/main/kotlin/ru/pnzgu/hackapp/controller/IndexController.kt
@@ -1,9 +1,13 @@
 package ru.pnzgu.hackapp.controller
 
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.pnzgu.hackapp.service.EventService
 
 @RestController
-class IndexController() {
+class IndexController(private val eventService : EventService) {
 
-
+    @GetMapping("/api/getallevents", MediaType.APPLICATION_JSON_VALUE)
+    fun getALlEventsApi() = eventService.getAllEvents()
 }
